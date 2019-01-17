aabee1a73172bdcc71479eac9b63caab411ee8ad
Kim Poulsen <4174578+kpoppel@users.noreply.github.com>
2019-01-17 21:33:44 +0100

    Add environment files, so local configuration can go into .env file.

--- dns/prepare.sh
+++ dns/prepare.sh
@@ -18,8 +18,8 @@ echo "Generating traefik rules"
 #echo "Getting root.hints"
 #wget -O config/unbound/root.hints https://www.internic.net/domain/named.root
 
-#echo "Setting extra fixed IP address to the server"
-#sudo ip addr add 10.0.1.128/23 dev wlan0
+#echo "Setting extra fixed IP address to the server - wired LAN"
+#sudo ip addr add 10.0.1.128/23 dev eth0
 
 echo "Go ahead and start the containers"
 echo "docker-compose up -d"
